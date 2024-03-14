57ee46f77391823ab5adbbc67bb9800222e134b0
EG-Boys <109102143+EG-Boys@users.noreply.github.com>
2024-03-15 00:10:54 +0600

    Update znoprefix.js

--- scripts/cmds/znoprefix.js
+++ scripts/cmds/znoprefix.js
@@ -45,7 +45,7 @@ case "prefix2":
           await api.setMessageReaction("🪐", event.messageID, event.threadID, api);
   case "owner":
 message.reply({
-body: "———»ADMIN BOT«———\n❯ Bot Name: yazky\n❯ Bot Owner: CLIFF VINCENT\n❯ Age: 18 \n❯ Gender: Male\n❯ Facebook: https://www.facebook.com/swordigo.swordslush\n❯ Total Group: 12\n❯ Total Users: 862\n❯ Bot Prefix: #\n❯ Today is: Monday,February 5, 2024 3:33 PM\n❯ Thanks for using YAZKY BOT",
+body: "———»ADMIN BOT«———\n❯ Bot Name: yazky\n❯ Bot Owner: Light_X_Rafi\n❯ Age: 15 \n❯ Gender: Male\n❯ Facebook: https://www.facebook.com/100077513002567\n❯ Total Group: 12\n❯ Total Users: 862\n❯ Bot Prefix: /\n❯ Today is: Monday,February 5, 2024 3:33 PM\n❯ Thanks for using Junpi AI",
             attachment: fs.createReadStream("scripts/cmds/noprefix/goodnight.gif"),
           });
           await api.setMessageReaction("😙", event.messageID, event.threadID, api);
